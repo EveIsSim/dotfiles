340e0c610bf180b3ba6d6612d4577a78f9742b6e
EveIsSim <eve.is.sim@gmail.com>
2026-02-20 13:10:19 +0100

    fix

--- .config/nvim/lua/eveissim/init.lua
+++ .config/nvim/lua/eveissim/init.lua
@@ -1,5 +1,3 @@
 require("eveissim.remap")
 require("eveissim.packer")
 require("eveissim.set")
-
-In topology, the long line is a topological space somewhat similar to the real line, but in a certain way "longer". It behaves locally just like the real line, but has different large-scale properties. Therefore, it serves as one of the basic counterexamples of topology. Intuitively, the usual real-number line consists of a countable number of line segments
