99846c4f757c5db3f80a92265651344eac8d3c63
EveIsSim <eve.is.sim@gmail.com>
2026-03-27 18:52:28 +0100

    add csharp to folding

--- .config/nvim/after/plugin/folding.lua
+++ .config/nvim/after/plugin/folding.lua
@@ -3,13 +3,27 @@ vim.opt.foldlevelstart = 99
 vim.opt.foldcolumn = "1"
 vim.opt.mouse = "a"
 
+local ts_fold_filetypes = {
+    "json",
+    "jsonc",
+    "yaml",
+    "javascript",
+    "typescript",
+    "markdown",
+    "go",
+    "lua",
+    "rust",
+    "sh",
+    "cs",
+}
+
 local ts_foldexpr = "nvim_treesitter#foldexpr()"
 if vim.treesitter and vim.treesitter.foldexpr then
     ts_foldexpr = "v:lua.vim.treesitter.foldexpr()"
 end
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "json", "jsonc", "yaml", "javascript", "typescript", "markdown", "go", "csharp", "lua", "rust" },
+    pattern = ts_fold_filetypes,
     callback = function()
         vim.opt_local.foldmethod = "expr"
         vim.opt_local.foldexpr = ts_foldexpr
